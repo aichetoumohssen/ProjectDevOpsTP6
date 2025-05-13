74ab9e96149ee2b58d2bfbf54081f7722f040b7e
Aichetou <aichetoumohssen@gmail.com>
2025-05-13 13:04:17 +0000

    Initial commit Spring Boot

---
+++ src/main/java/mr/esp/TP6DevOps/Tp6DevOpsApplication.java
@@ -0,0 +1,13 @@
+package mr.esp.TP6DevOps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp6DevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp6DevOpsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mr/esp/TP6DevOps/Tp6DevOpsApplicationTests.java
@@ -0,0 +1,13 @@
+package mr.esp.TP6DevOps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tp6DevOpsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
